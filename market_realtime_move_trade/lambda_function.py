1c7233b39e8c4ffb094808acd5250439823a64f7
Hyungjun Lim <sculd3@gmail.com>
2022-04-30 22:43:27 -0400

    market_trading_binance_kinesis

--- market_realtime_move_trade/lambda_function.py
+++ market_realtime_move_trade/lambda_function.py
@@ -51,6 +51,6 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps(start_task('trading', 'market_realtime_move_report_kinesis', 'market_realtime_move_report_kinesis:2',
-          ['java', '-jar', 'market_realtime_move_report_kinesis-1.0-SNAPSHOT.jar', '--shardid=0', '--apptype=changes_anomaly_trading']))
+        'body': json.dumps(start_task('trading', 'market_trading_binance_kinesis', 'market_trading_binance_kinesis:1',
+          ['java', '-jar', 'market_realtime_move_report_kinesis-1.0-SNAPSHOT.jar', '--shardid=0', '--apptype=changes_anomaly_trading_binance']))
     }
